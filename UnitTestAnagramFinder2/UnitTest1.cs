f40db7dd6e8eb92690d22a80a6ea188e6ea74ee8
slukenba <45136658+slukenba@users.noreply.github.com>
2018-11-18 00:30:50 -0500

    Add files via upload

---
+++ UnitTestAnagramFinder2/UnitTest1.cs
@@ -0,0 +1,29 @@
+using AnagramFinder2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestAnagramFinder2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSortAnagramCharacters()
+        {
+            var program = new Program();
+            string anagramToSort = "stop";
+            string sortedAnagram = program.SortAnagramCharacters(anagramToSort);
+
+            Assert.AreEqual("opst", sortedAnagram);
+        }
+
+        [TestMethod]
+        public void TestCreateDictValue()
+        {
+            var program = new Program();
+            string currentValue = "stop";
+            string newValue = "tops";
+
+            Assert.AreEqual("stop,tops", currentValue + "," + newValue);
+        }
+    }
+}
